1a2e51391c3651776cee88f2c5b7dd2557a5e80f
zhouhaipeng <zhouhaipeng@coldlar.com>
2019-06-19 15:01:05 +0800

    添加方法

--- index.js
+++ index.js
@@ -197,3 +197,14 @@ export const isBrowser = () => {
 		}
 	}
 }
+/**
+ * 随机字符串 
+ * 默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1
+ */
+export const randomString = (len = 10) {
+	const $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678',
+		maxPos = $chars.length
+	let pwd = ''
+	for (let i = 0; i < len; i++) pwd += $chars.charAt(Math.floor(Math.random() * maxPos))
+	return pwd
+}
