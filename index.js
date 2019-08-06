b5bee038bcd73cfbcc604cdd5258dd3901b3a048
Roc-zhou <13031902163@163.com>
2019-08-06 11:17:35 +0800

    修改日期格式化函数

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 /**
  * 采用Es6方式
+ * author:roc_zhou
  */
 
 /**
  * [时间格式化]
  * @param    {[String]}                 date [需要转换的时间]
- * @return   {[String]}                      [输出格式 2018-03-23 13:35:47]
+ * @param    {[String]}                 format [需要转换的日期格式]
+ * @return   {[String]}                      [默认输出格式 2018-03-23 13:35:47]
+ * 
+ * yyyy 年 2018
+ * MM 月份 03
+ * dd 天 23
+ * hh 小时 13
+ * mm 分钟 35
+ * ss 秒 47
  */
-const formatDate = date => {
-	date = new Date(date);
-	let y = date.getFullYear();
-	let m = date.getMonth() + 1;
-	let d = date.getDate();
-	let h = date.getHours();
-	let m1 = date.getMinutes();
-	let s = date.getSeconds();
-	m = m < 10 ? ("0" + m) : m;
-	d = d < 10 ? ("0" + d) : d;
-	h = h < 10 ? ("0" + h) : h;
-	m1 = m1 < 10 ? ("0" + m1) : m1;
-	s = s < 10 ? ('0' + s) : s;
-	return y + "-" + m + "-" + d + " " + h + ":" + m1 + ":" + s;
+const formatDate = (date, format = 'yyyy-MM-dd hh:mm:ss') => {
+    if (!date) return '';
+    date = new Date(date);
+    let y = date.getFullYear();
+    let m = date.getMonth() + 1;
+    let d = date.getDate();
+    let h = date.getHours();
+    let m1 = date.getMinutes();
+    let s = date.getSeconds();
+    m = m < 10 ? ("0" + m) : m;
+    d = d < 10 ? ("0" + d) : d;
+    h = h < 10 ? ("0" + h) : h;
+    m1 = m1 < 10 ? ("0" + m1) : m1;
+    s = s < 10 ? ('0' + s) : s;
+
+    return format = format.replace('yyyy', y).replace('MM', m).replace('dd', d).replace('hh', h).replace('mm', m1).replace('ss', s)
 }
 /**
  * [转换时间戳]
