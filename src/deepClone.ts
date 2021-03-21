66690afffa8d62eeb11ee9b4a009d9a04b8e4892
Roc-zhou <13031902163@163.com>
2021-03-21 16:06:08 +0800

    add clone

---
+++ src/deepClone.ts
@@ -0,0 +1,18 @@
+
+/**
+ * 深度克隆
+ */
+export function deepClone(obj: any): any {
+    if (obj === null) return null
+    if (typeof obj !== "object") return obj
+    if (obj instanceof RegExp) return new RegExp(obj)
+    if (obj instanceof Date) return new Date(obj)
+    // 不直接创建空对象目的：克隆的结果和之前保持相同类
+    let newObj = obj.constructor
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            newObj[key] = deepClone(obj[key])
+        }
+    }
+    return newObj
+}
