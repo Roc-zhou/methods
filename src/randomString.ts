d7c58ff5f4741e189577acc00d729ec11c829dac
Roc-zhou <13031902163@163.com>
2021-03-23 14:14:51 +0800

    add string methods

---
+++ src/randomString.ts
@@ -0,0 +1,11 @@
+/**
+ * 随机字符串
+ * 默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1
+ */
+export function randomString<T>(len: number = 10): string {
+    const $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678',
+        maxPos = $chars.length
+    let pwd = ''
+    for (let i = 0; i < len; i++) pwd += $chars.charAt(Math.floor(Math.random() * maxPos))
+    return pwd
+}
