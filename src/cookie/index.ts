93e66113644c102c564ea74f7dbcb7b1ea26d73e
Roc-zhou <13031902163@163.com>
2021-03-27 17:53:45 +0800

    add

---
+++ src/cookie/index.ts
@@ -0,0 +1,34 @@
+/**
+ * 获取cookie
+ */
+export function getCookie<T>(c_name: string) {
+    if (!!document.cookie.length) {
+        let c_start = document.cookie.indexOf(c_name + '=')
+        if (!!~c_start) {
+            c_start = c_start + c_name.length + 1
+            let c_end = document.cookie.indexOf(';', c_start)
+            if (!~c_end) c_end = document.cookie.length
+            return decodeURI(document.cookie.substring(c_start, c_end))
+        }
+    }
+    return null
+}
+/**
+ * 设置cookie
+ * ms 毫秒
+ */
+export function setCookie<T>(c_name: string, value: any, ms: any = null) {
+    const date = new Date().getTime() + ms
+    return document.cookie = c_name + '=' + encodeURI(value) + ((ms === null) ? '' : ';expires=' + new Date(date).toUTCString()), null
+}
+/**
+ * 删除cookie
+ * @param    {[string]}                 name [description]
+ */
+export function delCookie<T>(name: string) {
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval = getCookie(name);
+    if (cval != null)
+        document.cookie = name + "=" + cval + ";expires=" + exp.toUTCString();
+}
